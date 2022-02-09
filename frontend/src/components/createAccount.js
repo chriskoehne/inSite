dd1301957de10e21ce20ae32597a2a40df60a98d
Tom Appenzeller <tappenze@purdue.edu>
2022-02-08 21:13:09 -0500

    basic login fields, some pages, some routes

---
+++ frontend/src/components/createAccount.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+
+export default class CreateAccount extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Create Account</h1>
+            </div>
+        );
+    }
+}
---
+++ frontend/src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+            </div>
+        );
+    }
+}
