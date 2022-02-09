73709f96496994df721d29431c7a4323e5f08d4e
Tom Appenzeller <tappenze@purdue.edu>
2022-02-08 22:17:24 -0500

    added redirect on successful login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,27 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import { Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import Login from './components/login';
 import Home from './components/home';
 import CreateAccount from './components/createAccount';
 
 const App = () => {
+  const history = useHistory();
   return (
     <div>
        <Route exact path="/">
-         <Login />
+         <Login 
+         history = {history}/>
         </Route>
         <Route exact path="/home">
-         <Home />
+         <Home 
+         history = {history}/>
         </Route>
         <Route exact path="/createAccount">
-         <CreateAccount />
+         <CreateAccount 
+         history = {history}/>
         </Route>
      </div>
   );
