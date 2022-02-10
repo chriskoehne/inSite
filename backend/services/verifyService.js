e1260f8645284e35e306e6c108ae52c233c58030
Tom Appenzeller <tappenze@purdue.edu>
2022-02-09 19:37:24 -0500

    a correct code is correctly validated. An incorrect code crashes the app and still allows the redirection

--- backend/services/verifyService.js
+++ backend/services/verifyService.js
@@ -7,33 +7,39 @@ const authToken = config.TwilioAuthToken;
 const User = require(path.resolve(__dirname, "../database/models/user"));
 exports.check = async function (req, res) {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const email = req.body.email;
     const code = req.body.code;
 
     let result = await User.find({ email: email });
     var authy = require("authy")(authToken);
-    console.log(email)
-    console.log(code)
-    console.log(result[0])
-    console.log("verifying code")
-    await authy.verify(result[0].authyId, token=code).then(function (authyres) {
-      // if (err) {
-      //   console.log("error caught")
-      //   res.status(400).send({ message: "invalid code" });
-      //   return;
-      // }
-      console.log(authyres)
-      if (authyres) {
-        if (authyres.success) {
-          console.log("correct code")
-        return
+    console.log(email);
+    console.log(code);
+    console.log(result[0]);
+    console.log("verifying code");
+    await authy.verify(
+      result[0].authyId,
+      (token = code),
+      function (err, authyres) {
+        // if (err) {
+        //   console.log("error caught")
+        //   res.status(400).send({ message: "invalid code" });
+        //   return;
+        // }
+        // console.log(authyres);
+        if (!authyres) {
+          callback("verify failed for some reason?");
+          res.status(400).send({ message: "invalid code" });
+          return;
+        } else {
+          console.log(authyres)
+          if (authyres.success) {
+            console.log("correct code");
+          }
+          return;
         }
-      } else {
-        res.status(400).send({ message: "invalid code" });
-        return;
       }
-    });
+    );
     return;
   } catch (err) {
     return err;
