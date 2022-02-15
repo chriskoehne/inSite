a99ea227e3d332308d3046241d9d338df93ea6eb
Chris Koehne <cdkoehne@gmail.com>
2022-02-14 22:22:47 -0500

    converted login and signup to functional components to accomodate useNavigate. Also changed https script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,24 @@
-import React from "react";
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
-import { Route } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import Login from './components/login';
 import Home from './components/home';
 import CreateAccount from './components/createAccount';
 
 const App = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
   return (
-    <div>
-       <Route exact path="/">
-         <Login 
-         history = {history}/>
-        </Route>
-        <Route exact path="/home">
-         <Home 
-         history = {history}/>
-        </Route>
-        <Route exact path="/createAccount">
-         <CreateAccount 
-         history = {history}/>
-        </Route>
-     </div>
+    <Routes>
+      <Route path='/' element={<Login navigate={navigate} />} />
+      <Route path='/home' element={<Home navigate={navigate} />} />
+      <Route
+        path='/createAccount'
+        element={<CreateAccount navigate={navigate} />}
+      />
+    </Routes>
   );
 };
 
--- frontend/src/components/createAccount.js
+++ frontend/src/components/createAccount.js
@@ -1,160 +1,132 @@
-import React, { Component } from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
-import Popup from 'reactjs-popup';
-import axios from "axios";
+import React, { useState } from 'react';
+// import { Button, Container, Row, Col } from "react-bootstrap";
+// import Popup from 'reactjs-popup';
+import axios from 'axios';
 import { Modal } from 'react-bootstrap';
 
-export default class CreateAccount extends Component {
-  constructor(props) {
-    super(props);
+const CreateAccount = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showModal, setShowModal] = useState('');
+  const [phone, setPhone] = useState('');
+  const [smsCode, setSMSCode] = useState('');
 
-    this.onChangeEmail = this.onChangeEmail.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-    this.onChangePhone = this.onChangePhone.bind(this);
-    this.onChangeSMSCode = this.onChangeSMSCode.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      email: "",
-      password: "",
-      phone: "",
-      showModal: false,
-      smsCode: ""
-    };
-  }
-
-  onChangeEmail(e) {
-    this.setState({
-      email: e.target.value,
-    });
-  }
-
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value,
-    });
-  }
-
-  onChangePhone(e) {
-      this.setState({
-          phone: e.target.value,
-      });
-  }
-
-  onChangeSMSCode(e) {
-      this.setState({
-          smsCode: e.target.value,
-      });
-  }
-
-  handleClose(e) {
+  const handleClose = (e) => {
     e.preventDefault();
-    console.log("verifying sms code");
+    console.log('verifying sms code');
     const body = {
-      email: this.state.email,
-      code: this.state.smsCode
+      email: email,
+      code: smsCode,
     };
-    axios.post("http://localhost:5000/verifyUser/", body).then((res) => {
-    if (res.status == 200) {
-      this.props.history.push('/home');
-      
-    } else {
-        console.log("incorrect code")
-    }
-  });
-  }
+    axios.post('http://localhost:5000/verifyUser/', body).then((res) => {
+      if (res.status === 200) {
+        props.navigate('/home');
+      } else {
+        console.log('incorrect code');
+      }
+    });
+  };
 
-  onSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     //axios stuff
     const body = {
-      email: this.state.email,
-      password: this.state.password,
-      phone: this.state.phone,
+      email: email,
+      password: password,
+      phone: phone,
     };
 
-    axios.post("http://localhost:5000/userCreation/", body).then((res) => {
-      if (res.status == 200) {
-        console.log("the modal should popup now");
+    axios.post('http://localhost:5000/userCreation/', body).then((res) => {
+      if (res.status === 200) {
+        console.log('the modal should popup now');
 
-        this.setState({
-          showModal: true
-        });
-        
+        setShowModal(true);
       } else {
-          console.log("there was an error in user creation")
+        console.log('there was an error in user creation');
       }
     });
-  }
+  };
 
-  render() {
-    
-    return (
-      <div>
-        <h1>Create Account</h1>
-        <Modal show={this.state.showModal}>
+  return (
+    <div>
+      <h1>Create Account</h1>
+      <Modal show={showModal}>
         <Modal.Header>
           <Modal.Title>Verify phone number</Modal.Title>
         </Modal.Header>
-        <Modal.Body>The phone number provided was used to create a two factor user. Please enter the code sent to your phone to verify this connection.
-        <form onSubmit={this.handleClose}>
-          <div className="form-group">
-            <label>Code: </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Code"
-              onChange={this.onChangeSMSCode}
-            />
-          </div>
-          <br></br>
-          <div className="form-group">
-            <input type="submit" value="Submit Code" className="btn btn-primary" />
-          </div>
-        </form>
+        <Modal.Body>
+          The phone number provided was used to create a two factor user. Please
+          enter the code sent to your phone to verify this connection.
+          <form onSubmit={handleClose}>
+            <div className='form-group'>
+              <label>Code: </label>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Code'
+                onChange={(e) => {
+                  setSMSCode(e.target.value);
+                }}
+              />
+            </div>
+            <br></br>
+            <div className='form-group'>
+              <input
+                type='submit'
+                value='Submit Code'
+                className='btn btn-primary'
+              />
+            </div>
+          </form>
         </Modal.Body>
-        {/* <Modal.Footer>
-          <Button variant="primary" onClick={this.handleClose}>
-            Submit
-          </Button>
-        </Modal.Footer> */}
       </Modal>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Email: </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="email"
-              onChange={this.onChangeEmail}
-            />
-          </div>
-          <div className="form-group">
-            <label>Password: </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="password"
-              onChange={this.onChangePassword}
-            />
-          </div>
-          <div className="form-group">
-            <label>Phone: </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="phone"
-              onChange={this.onChangePhone}
-            />
-          </div>
-          <br></br>
-          <div className="form-group">
-            <input type="submit" value="Create Account" className="btn btn-primary" />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label>Email: </label>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='email'
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div className='form-group'>
+          <label>Password: </label>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='password'
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <div className='form-group'>
+          <label>Phone: </label>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='phone'
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+          />
+        </div>
+        <br></br>
+        <div className='form-group'>
+          <input
+            type='submit'
+            value='Create Account'
+            className='btn btn-primary'
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateAccount;
+
