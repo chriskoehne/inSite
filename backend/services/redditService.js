4a11ccbdbcb41ca0644ec3d2a58843c9b624c788
JacksonOriez <jackson.oriez@sbcglobal.net>
2022-02-16 19:47:22 -0500

    working on reddit login setup

--- backend/services/redditService.js
+++ backend/services/redditService.js
@@ -1,23 +1,9 @@
 const path = require("path");
 const bcrypt = require("bcrypt");
-const Reddit = require('reddit')
+const Reddit = require('reddit');
+const { link } = require("fs");
 const config = require(path.resolve(__dirname, "../config.json"));
 const User = require(path.resolve(__dirname, "../database/models/user"));
-
-const randomString = createRandomString();
-
-function createRandomString() {
-  var result = ""
-  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  var charsLength = chars.length;
-
-  for ( var i = 0; i < 10; i++ ) {
-    result += chars.charAt(Math.floor(Math.random() * 
-    charsLength));
-  }
-
-  return result;
-}
 
 exports.test = async function (req, res) {
   try {
@@ -42,41 +28,38 @@ exports.test = async function (req, res) {
   }
 };
 
-exports.getUser = async function (req, res) {
-    try {
-      // console.log(req.body);
-      const email = req.body.email;
-      let result = await User.find({ email: email });
-  
-      return result;
-      
-  
-    } catch (err) {
-      console.log("big error catch")
-      return err;
-    }
-  };
-
   exports.login = async function (req, res) {
     try {
+  
       // console.log(req.body);
-      const email = req.body.email;
-      const redditEmail = req.body.socEmail;
-      const redditPassword = req.body.socPassword;
-      //console.log("In Login");
+      console.log("In Login");
+      console.log(req.body)
       // let result = await User.find({ email: email });
       //TODO use reddit api to verify user
 
-      link = "https://www.reddit.com/api/v1/authorize?client_id=9Hu_S9TRq-MdOuPUvYe3Vw&response_type=code&state=" + randomString + 
-      "&redirect_uri=http://127.0.0.1:3000/dashboard&duration=temporary&scope=subscribe,vote,mysubreddits,save,read,privatemessages,identity,account,history"
+      // const randomString = ""
+      // var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      // var charsLength = chars.length;
 
+      // for ( var i = 0; i < 10; i++ ) {
+      //   randomString += chars.charAt(Math.floor(Math.random() * 
+      //   charsLength));
+      // }
+
+      const randomString = req.body.email
+
+
+      const link = "https://www.reddit.com/api/v1/authorize?client_id=9Hu_S9TRq-MdOuPUvYe3Vw&response_type=code&state=" + randomString + 
+      "&redirect_uri=https://127.0.0.1:3000/dashboard/&duration=temporary&scope=subscribe,vote,mysubreddits,save,read,privatemessages,identity,account,history";
+
+      console.log("link");
       console.log(link);
 
-      let result = await User.findOneAndUpdate(
-        {"email": email}, 
-        {"redditUsername": redditEmail, "redditPassword": redditPassword},
-        );
-      return result;
+      // let result = await User.findOneAndUpdate(
+      //   {"email": email}, 
+      //   {"redditUsername": redditEmail, "redditPassword": redditPassword},
+      //   );
+      return {link: link, verificationString: randomString};
       
   
     } catch (err) {
--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -10,25 +10,31 @@ const Dashboard = (props) => {
 
   const { state } = useLocation();
   const [redditSuccess, setRedditSuccess] = useState(false);
-  const [redditLoading, setRedditLoading] = useState(true);
   // const email = props.navigate.arguments.email || 'Invalid login occurred'
   
-  useEffect(() => {
-    getRedditStatus();
-  }, []);
+  // console.log("logging")
+  // console.log(props)
+  // console.log(params)
 
-  const getRedditStatus = () => {
-    const body = {
-      email: state.email,
-    };
-    axios.get("http://localhost:5000/redditUser/", body).then((res) => {
-    setRedditSuccess(res.data.success)  
-    setRedditLoading(false);
-    });
-  };
+  const getEmail = () => {
+    if (state) {
+      return state.email
+    } else {
+      setRedditSuccess(true);
+      const currentUrl = window.location.href;
+      let start = currentUrl.indexOf('state') + 6
+      const after = currentUrl.substring(start)
+      let end = after.indexOf('&');
+      let almost = after.substring(0, end)
+      let email = almost.replace('%40', '@')
+      console.log(email)
+      return email
+      //get it from the url
+    }
+  }
   
 //clunky, but follow the above and add to the following if statements for the other social medias
-if (!redditLoading) {
+
   return (
     <div className={styles.box}>
       <Navbar className={styles.dashboardNav}>
@@ -47,16 +53,14 @@ if (!redditLoading) {
       </Navbar>
       
       <Row xs={1} md={2} className={styles.cardRow}>
-        <InsightCard title='Reddit' text='Reddit' isLoggedIn={redditSuccess} email={state.email}/>
+        <InsightCard title='Reddit' text='Reddit' isLoggedIn={redditSuccess} email={getEmail()} navigate={props.navigate} setExternalUrl={props.setExternalUrl}/>
         <InsightCard title='Twitter' text='put Twitter stuff here' />
         <InsightCard title='Instagram' text='put Instagram stuff here' />
         <InsightCard title='YouTube' text='put YouTube stuff here' />
       </Row>
     </div>
   );
-} else {
-  return null
-}
+
 };
 
 export default Dashboard;
