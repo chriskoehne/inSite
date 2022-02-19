45c4852ce64372f5e5c8534de79c1d3bc7da19c0
Chris Koehne <cdkoehne@gmail.com>
2022-02-19 14:55:47 -0500

    use only https for cookies now

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin: ['http://localhost:3000', 'https://localhost:3000'],
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
