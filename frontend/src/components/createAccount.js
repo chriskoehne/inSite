9f5f05ec37f6f51be436dc0eae24e2f8ce7e6b07
rayye1 <54600160+rayye1@users.noreply.github.com>
2022-02-20 20:58:05 -0500

    Create Account and Login Checks
    - Add warning messages for login failure
    - Add warning messages for Email Failure and Password Failure in create account

--- frontend/src/components/createAccount.js
+++ frontend/src/components/createAccount.js
@@ -13,6 +13,8 @@ const CreateAccount = (props) => {
   const [phone, setPhone] = useState('');
   const [formatPhone, setFormatPhone] = useState(''); //Different from phone (Visual Purposes Only)
   const [smsCode, setSMSCode] = useState('');
+  const [errorText, setErrorText] = useState('');
+  const [showErrorModal, setErrorModal] = useState('')
 
   useEffect(() => {
     async function callUnauthedOnly() {
@@ -20,6 +22,8 @@ const CreateAccount = (props) => {
     }
     callUnauthedOnly();
   }, []);
+
+  const handleCloseError = () => setErrorModal(false) // Handles Error Modal Close
 
   const handleClose = (e) => {
     e.preventDefault();
@@ -46,6 +50,18 @@ const CreateAccount = (props) => {
       password: password,
       phone: phone,
     };
+    console.log("lasdas")
+    if (!email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )) {
+      setErrorText('Email is not valid')
+      setErrorModal(true)
+      return ;
+    }
+    if (password.length < 8) {
+      setErrorText('Password has to be longer than 7 characters')
+      setErrorModal(true)
+      return ;
+    }
 
     axios.post('http://localhost:5000/userCreation/', body).then((res) => {
       if (res.status === 200) {
@@ -54,6 +70,12 @@ const CreateAccount = (props) => {
         setShowModal(true);
       } else {
         console.log('there was an error in user creation');
+      }
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response.data.message)
+        setErrorModal(true);
+        setErrorText(error.response.data.message)
       }
     });
   };
@@ -106,6 +128,14 @@ const CreateAccount = (props) => {
                   />
                 </div>
               </form>
+            </Modal.Body>
+          </Modal>
+          <Modal show={showErrorModal} onHide={handleCloseError}>
+            <Modal.Header closeButton>
+              <Modal.Title>SIGNUP ERROR</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {errorText}
             </Modal.Body>
           </Modal>
           <form onSubmit={handleSubmit}>
