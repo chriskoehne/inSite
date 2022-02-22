0e75a9e2d10524fba3584ec7dbcc11a2b316ce39
Tom Appenzeller <tappenze@purdue.edu>
2022-02-22 16:28:39 -0500

    went ahead and also added cards for comments and most upvoted messages, comments, and posts

--- frontend/src/components/Reddit/RedditPage.js
+++ frontend/src/components/Reddit/RedditPage.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Container, Navbar, Row, Card, Col, Carousel } from 'react-bootstrap';
-import { useLocation } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import LineChart from '../Charts/LineChart';
-import BarChart from '../Charts/BarChart';
-import styles from './Reddit.module.css';
-const c = require('./constants/constants');
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Navbar, Row, Card, Col, Carousel } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LineChart from "../Charts/LineChart";
+import BarChart from "../Charts/BarChart";
+import styles from "./Reddit.module.css";
+const c = require("./constants/constants");
 
 const RedditPage = (props) => {
   const { state } = useLocation();
@@ -20,17 +20,17 @@ const RedditPage = (props) => {
 
   useEffect(() => {
     // Update the document title using the browser API
-    console.log('going to attempt to use access token now');
+    console.log("going to attempt to use access token now");
     console.log(state);
     const redditMeQuery = {
       accessToken: state.accessToken,
     };
     if (me && !me.name) {
       axios
-        .get('http://localhost:5000/redditMe', { params: redditMeQuery })
+        .get("http://localhost:5000/redditMe", { params: redditMeQuery })
         .then((ans) => {
           if (ans) {
-            console.log('me request ans - see data name');
+            console.log("me request ans - see data name");
             console.log(ans);
             setMe(ans.data);
             //because me contains vital information, such as a username, maybe we should nest all of the calls? or perhaps get one big blob of data from one backend call?
@@ -39,18 +39,18 @@ const RedditPage = (props) => {
               username: ans.data.name,
             };
             axios
-              .get('http://localhost:5000/redditUserOverview', {
+              .get("http://localhost:5000/redditUserOverview", {
                 params: redditUserQuery,
               })
               .then((ans) => {
                 if (ans) {
-                  console.log('overview request ans - see data');
+                  console.log("overview request ans - see data");
                   // console.log(ans.data.overview.data);
                   //ans.data.overview.data.children <- a list of objects. Look at 'kind' field
-                  console.log(ans)
-                  setComments(ans.data.comments)
-                  setPosts(ans.data.posts)
-                  setMessages(ans.data.messages)
+                  console.log(ans);
+                  setComments(ans.data.comments);
+                  setPosts(ans.data.posts);
+                  setMessages(ans.data.messages);
                 }
               });
           }
@@ -62,16 +62,26 @@ const RedditPage = (props) => {
     var maxScore = 0;
     list.forEach(function (item, index) {
       if (item.score > maxScore) {
-        maxScore = item.score
+        maxScore = item.score;
       }
     });
     return maxScore;
   };
 
+  const getMaxItem = (list, maxScore) => {
+    var maxItem = {};
+    list.forEach(function (item, index) {
+      if (item.score === maxScore) {
+        maxItem = item;
+      }
+    });
+    return maxItem;
+  };
+
   const getScores = (list) => {
     let scores = [];
     list.forEach(function (item, index) {
-      scores.push(item.score)
+      scores.push(item.score);
     });
     return scores;
   };
@@ -85,21 +95,21 @@ const RedditPage = (props) => {
   return !me || !me.name ? (
     <div
       style={{
-        width: '100vw',
-        height: '100vh',
-        background: 'white',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        textAlign: 'center',
+        width: "100vw",
+        height: "100vh",
+        background: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
       }}
     >
-      <h1 style={{ color: '#3d3d3d' }}>Loading...</h1>
+      <h1 style={{ color: "#3d3d3d" }}>Loading...</h1>
     </div>
   ) : (
     <div className={styles.box}>
       <Carousel
-        variant='dark'
+        variant="dark"
         className={styles.slideshow}
         activeIndex={index}
         onSelect={handleSelect}
@@ -109,10 +119,92 @@ const RedditPage = (props) => {
             <Row>
               <Col>
                 <Row className={styles.chartContainer}>
-                  <BarChart data={getScores(posts)} maxVal={getMaxScore(posts)} label="Post Scores" xaxis="post score"/>
+                  <BarChart
+                    data={getScores(posts)}
+                    maxVal={getMaxScore(posts)}
+                    label="Post Scores"
+                    xaxis="post score"
+                  />
                 </Row>
               </Col>
-              <Col>Here we see a graphical representation of a user's post scores. Each bucket represents the number of posts that fall within the range for the score.</Col>
+              <Col>
+                Here we see a graphical representation of a user's post scores.
+                Each bucket represents the number of posts that fall within the
+                range for the score.
+              </Col>
+            </Row>
+          </Card>
+        </Carousel.Item>
+        <Carousel.Item className={styles.slideshowCard}>
+          <Card className={styles.socialsCard}>
+            <Row>
+              <Col>
+                <Row className={styles.chartContainer}>
+                  <BarChart
+                    data={getScores(comments)}
+                    maxVal={getMaxScore(comments)}
+                    label="Comment Scores"
+                    xaxis="Comment score"
+                  />
+                </Row>
+              </Col>
+              <Col>
+                Here we see a graphical representation of a user's comment
+                scores. Each bucket represents the number of comments that fall
+                within the range for the score.
+              </Col>
+            </Row>
+          </Card>
+        </Carousel.Item>
+        <Carousel.Item className={styles.slideshowCard}>
+          <Card className={styles.socialsCard}>
+            <Row>
+              Most Upvoted Post
+              <Card
+                style={{ borderColor: "#3d3d3d" }}
+                // className={styles.socialsCard}
+              >
+                <Card.Body>
+                  <Card.Title>
+                    {getMaxItem(posts, getMaxScore(posts)).title}
+                  </Card.Title>
+                  <Card.Text>
+                    {getMaxItem(posts, getMaxScore(posts)).selftext}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Row>
+            <Row>
+              Most Upvoted Comment
+              <Card
+                style={{ borderColor: "#3d3d3d" }}
+                // className={styles.socialsCard}
+              >
+                <Card.Body>
+                <Card.Title>
+                    {getMaxItem(comments, getMaxScore(comments)).link_title}
+                  </Card.Title>
+                  <Card.Text>
+                    {getMaxItem(comments, getMaxScore(comments)).body}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Row>
+            <Row>
+              Most Upvoted Message
+              <Card
+                style={{ borderColor: "#3d3d3d" }}
+                // className={styles.socialsCard}
+              >
+                <Card.Body>
+                <Card.Title>
+                    {getMaxItem(messages, getMaxScore(messages)).link_title}
+                  </Card.Title>
+                  <Card.Text>
+                    {getMaxItem(messages, getMaxScore(messages)).body}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
             </Row>
           </Card>
         </Carousel.Item>
