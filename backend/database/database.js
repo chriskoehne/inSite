57c1504c683fa9b51306e35cf05f1adbbfe3d9f3
Chris Koehne <cdkoehne@gmail.com>
2022-03-23 15:18:37 -0400

    trying to figure out where console logs are

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const path = require("path");
 const uri = process.env.DATABASE_URI;
+console.log('uri is')
+console.log(uri)
 
 mongoose.connect(uri, {
     useNewUrlParser: true}).then(
