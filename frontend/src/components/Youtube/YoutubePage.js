ed21184a69258080168b0f575fe6b745a8c5b074
Tom Appenzeller <tappenze@purdue.edu>
2022-03-27 15:20:57 -0400

    basic working youtube page

--- frontend/src/components/Youtube/YoutubePage.js
+++ frontend/src/components/Youtube/YoutubePage.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { Row, Card } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Row, Card, Col, Carousel, Button, ButtonGroup } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './Youtube.module.css';
 
 const YoutubePage = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [index, setIndex] = useState(0);
 
-  return (
+
+  const isDarkMode = () => {
+    return document.body.classList.contains('dark') ? 'light' : 'dark';
+  };
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return loading ? (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <h1 style={{ color: 'var(--primary)' }}>Loading...</h1>
+    </div>
+  ) : (
     <div className={styles.box}>
-
-      <h1>Youtube Page</h1>
-      <Row xs={1} md={2} className={styles.cardRow}>
-      <Card className={styles.socialsCard}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>Card Text</Card.Text>
-          Display a chart
-        </Card.Body>
-      </Card>
-      </Row>
+      <Carousel
+        variant={isDarkMode()}
+        className={styles.slideshow}
+        activeIndex={index}
+        onSelect={handleSelect}
+      >
+        <Carousel.Item className={styles.slideshowCard}>
+          <Card className={styles.socialsCard}>
+            <Row>
+              <Col>
+                <Row>Smthg youtube related</Row>
+              </Col>
+            </Row>
+          </Card>
+        </Carousel.Item>
+        <Carousel.Item className={styles.slideshowCard}>
+          <Card className={styles.socialsCard}>
+            <h3>your mom</h3>
+          </Card>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 
