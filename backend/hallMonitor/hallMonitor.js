72a8b30506e227e85647244e392ec94814e420d9
Tom Appenzeller <tappenze@purdue.edu>
2022-04-23 17:33:57 -0400

    youtube refreshes on its own?

--- backend/hallMonitor/hallMonitor.js
+++ backend/hallMonitor/hallMonitor.js
@@ -4,6 +4,7 @@ var redditService = require('../services/redditService'); // skipping over the c
 
 exports.monitor = async function (email, password) {
     console.log("monitoring")
+    console.log(Date.now())
     let users = await User.find({});
     users.forEach(async (user) => {
         console.log("checking: " + user.email)
@@ -13,6 +14,12 @@ exports.monitor = async function (email, password) {
                 // call refresh using token
                 let result = await redditService.refresh(user.reddit.refresh_token, user.email); 
     
+            }
+        }
+        if (user.youtube) {
+            if (user.youtube.expiry_date <= Date.now()) {
+                //refresh the token
+                console.log("youtube is expired. Does it still work on its own?")
             }
         }
         //youtube refreshes on its own using the oauthclient
